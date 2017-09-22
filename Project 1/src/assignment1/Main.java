f6b827ccc6893237b7e8ea512705e6a5a7793d68
HaydenBrewBrew <haydenlydick@utexas.edu>
2017-09-22 01:54:04 -0500

    a

---
+++ Project 1/src/assignment1/Main.java
@@ -0,0 +1,115 @@
+/* 
+ * This file is how you might test out your code.  Don't submit this, and don't 
+ * have a main method in SortTools.java.
+ */
+
+package assignment1;
+import java.util.Arrays;
+public class Main {
+	public static void main(String [] args) {
+
+		//test everything except time complexity
+        int[] e = {};
+        int [] a = {1};
+        int [] aa = {2,1};//multiple instances
+        int [] b = {0,1,7,4,2,1,7,5,2,1,45,6,8,4,1,1,4,6,8,34,6,6,4}; //multiple instances
+        int [] c = {0,1,2,3,4,5,6,7,8,9};//in order
+        int [] d = {9,8,7,6,5,4,3,2,1}; //in reserve order
+        System.out.println("~~~~~~ISSORTED~~~~~~");
+        System.out.println("false: " + SortTools.isSorted(e, 0));
+        System.out.println("false: " + SortTools.isSorted(a, 0));
+        System.out.println("false: " + SortTools.isSorted(b, 0));
+        System.out.println("true: " + SortTools.isSorted(a, 1));
+        System.out.println("false: " + SortTools.isSorted(aa, 2));
+        System.out.println("true: " + SortTools.isSorted(b, 2));
+        System.out.println("false: " + SortTools.isSorted(b, b.length));
+        System.out.println("true: " + SortTools.isSorted(c, c.length));
+        System.out.println("true: " + SortTools.isSorted(c, 5));
+        System.out.println("false: " + SortTools.isSorted(d, d.length));
+        System.out.println("false: " + SortTools.isSorted(d, d.length-3));
+        
+        System.out.println("\n~~~~~~INSERTSORT~~~~~~");
+        SortTools.insertSort(a, 1);
+        System.out.println("[1]: " + Arrays.toString(a));
+        SortTools.insertSort(aa, 2);
+        System.out.println("[1,2]: " + Arrays.toString(aa));
+        SortTools.insertSort(b, 10);
+        System.out.println("[0,1,1,1,1,1,2,2,5,7 + the rest]: " + Arrays.toString(b));
+        SortTools.insertSort(b, b.length);
+        System.out.println("[0,1,1,1,1,1,2,2,4,4,4,4,5,6,6,6,6,7,7,8,8,34,45]: " + Arrays.toString(b));
+        SortTools.insertSort(c, 6);
+        System.out.println("[0,1,2,3,4,5,6,7,8,9]: " + Arrays.toString(c));
+        SortTools.insertSort(c, c.length);
+        System.out.println("[0,1,2,3,4,5,6,7,8,9]: " + Arrays.toString(c));
+        SortTools.insertSort(c, c.length);
+        System.out.println("[0,1,2,3,4,5,6,7,8,9]: " + Arrays.toString(c));
+        SortTools.insertSort(d, d.length);
+        System.out.println("[1,2,3,4,5,6,7,8,9]: " + Arrays.toString(d));
+       
+        System.out.println("\n~~~~~~FIND~~~~~~");
+        System.out.println("-1: " + SortTools.find(a, 1, 0));
+        System.out.println("0: " + SortTools.find(a, 1, 1));
+        System.out.println("0: " + SortTools.find(aa, 2, 1));
+        System.out.println("1: " + SortTools.find(aa, 2, 2));
+        System.out.println("-1: " + SortTools.find(b, 10, 5));
+        System.out.println("6 or 7: " + SortTools.find(b, 10, 2));
+        System.out.println("12: " + SortTools.find(b, b.length, 5));
+        System.out.println("0: " + SortTools.find(b, b.length, 0));
+        System.out.println( b.length -1 +": " + SortTools.find(b, b.length, 45));
+        System.out.println("-1: " + SortTools.find(b, b.length, 46));
+        System.out.println("22: " + SortTools.find(b, b.length, 45));
+        System.out.println("5: " + SortTools.find(c, c.length, 5));
+        System.out.println("-1: " + SortTools.find(c, c.length, 50));
+        
+        System.out.println("\n~~~~~~INSERTGENERAL~~~~~~");
+        int [] x = SortTools.insertGeneral(a, 0, 1);
+        System.out.println("[1]: " + Arrays.toString(x));
+        x = SortTools.insertGeneral(a, 1, 1);
+        System.out.println("[1]: " + Arrays.toString(x));
+        x = SortTools.insertGeneral(a, 1, 2);
+        System.out.println("[1,2]: " + Arrays.toString(x));
+        x = SortTools.insertGeneral(b, 0, 3);
+        System.out.println("[3]: " + Arrays.toString(x));
+        x = SortTools.insertGeneral(b, 10, 1);
+        System.out.println("[0,1,1,1,1,1,2,2,4,4]: " + Arrays.toString(x));
+        x = SortTools.insertGeneral(b, 10, 3);
+        System.out.println("[0,1,1,1,1,1,2,2,3,4,4]: " + Arrays.toString(x));
+        x = SortTools.insertGeneral(b, 10, 5);
+        System.out.println("[0,1,1,1,1,1,2,2,4,4,5]: " + Arrays.toString(x));
+        x = SortTools.insertGeneral(b, b.length, 0);
+        System.out.println("[0,1,1,1,1,1,2,2,4,4,4,4,5,6,6,6,6,7,7,8,8,34,45]: " + Arrays.toString(x));
+        x = SortTools.insertGeneral(b, b.length, 4);
+        System.out.println("[0,1,1,1,1,1,2,2,4,4,4,4,5,6,6,6,6,7,7,8,8,34,45]: " + Arrays.toString(x));
+        x = SortTools.insertGeneral(b, b.length, 45);
+        System.out.println("[0,1,1,1,1,1,2,2,4,4,4,4,5,6,6,6,6,7,7,8,8,34,45]: " + Arrays.toString(x));
+        x = SortTools.insertGeneral(b, b.length, 10);
+        System.out.println("[0,1,1,1,1,1,2,2,4,4,4,4,5,6,6,6,6,7,7,8,8,10,34,45]: " + Arrays.toString(x));
+        x = SortTools.insertGeneral(b, b.length, -5);
+        System.out.println("[-5,0,1,1,1,1,1,2,2,4,4,4,4,5,6,6,6,6,7,7,8,8,34,45]: " + Arrays.toString(x));
+        x = SortTools.insertGeneral(b, b.length, 46);
+        System.out.println("[0,1,1,1,1,1,2,2,4,4,4,4,5,6,6,6,6,7,7,8,8,34,45,46]: " + Arrays.toString(x));
+                
+        System.out.println("\n~~~~~~INSERTINPLACE~~~~~~");
+        int al = SortTools.insertInPlace(a, 1,1);
+        System.out.println("[1] 1: " + Arrays.toString(a)+ " " + al);
+        al = SortTools.insertInPlace(aa, 1,2);
+        System.out.println("[1,2] 2: " + Arrays.toString(aa)+ " " + al);
+        al = SortTools.insertInPlace(aa, 1, 0);
+        System.out.println("[0,1] 2: " + Arrays.toString(aa)+ " " + al);
+        al = SortTools.insertInPlace(b, b.length-1, 0);
+        System.out.println("[0,1,1,1,1,1,2,2,4,4,4,4,5,6,6,6,6,7,7,8,8,34,45] "+ (b.length-1)  + ": " + Arrays.toString(b)+ " " + al);
+        al = SortTools.insertInPlace(b, b.length-1, 3);
+        System.out.println("[0,1,1,1,1,1,2,2,3,4,4,4,4,5,6,6,6,6,7,7,8,8,34] "+ (b.length)  + ": " + Arrays.toString(b)+ " " + al);
+        al = SortTools.insertInPlace(b, b.length-1, 46);
+        System.out.println("[0,1,1,1,1,1,2,2,3,4,4,4,4,5,6,6,6,6,7,7,8,8,34,46] "+ (b.length)  + ": " + Arrays.toString(b)+ " " + al);
+        al = SortTools.insertInPlace(b, b.length-1, 46);
+        System.out.println("[0,1,1,1,1,1,2,2,3,4,4,4,4,5,6,6,6,6,7,7,8,8,34,46] "+ (b.length)  + ": " + Arrays.toString(b)+ " " + al);
+        al = SortTools.insertInPlace(b, 10, 5);
+        System.out.println("[0,1,1,1,1,1,2,2,3,4,5,DONT CARE-->4,4,5,6,6,6,6,7,7,8,8,34,45] "+ 11  + ": " + Arrays.toString(b)+ " " + al);
+        al = SortTools.insertInPlace(b, 10, -5);
+        System.out.println("[-5,0,1,1,1,1,1,2,2,3,4,DONT CARE-->4,4,5,6,6,6,6,7,7,8,8,34,45] "+ 11  + ": " + Arrays.toString(b)+ " " + al);
+        al = SortTools.insertInPlace(b, 10, -5);
+        System.out.println("[-5,0,1,1,1,1,1,2,2,3,DONT CARE-->4,4,5,6,6,6,6,7,7,8,8,34,45] "+ 10  + ": " + Arrays.toString(b)+ " " + al);
+                  
+	}
+}
---
+++ Project 1/src/assignment1/SortTools.java
@@ -0,0 +1,147 @@
+// SortTools.java 
+/*
+ * EE422C Project 1 submission by Hayden Lydick
+ * Hayden Lydick
+ * hbl297
+ * 16290
+ * Spring 2017
+ * Slip days used: 0
+ */
+
+package assignment1;
+import java.util.Arrays;
+public class SortTools {
+	/**
+	  * This method tests to see if the given array is sorted.
+	  * @param x is the array
+	  * @param n is the size of the input to be checked
+	  * @return true if array is sorted
+	  */
+	public static boolean isSorted(int[] x, int n) {
+		if(n<=0){ //solves edge condition of n=0
+			return false;
+		}
+		//Checks if a number in the array is greater than the next number in the array
+		for(int i = 0; i < n - 1; i++ ){
+			if(x[i] > x[i+1]){ 			
+				return false;			
+			}
+		}
+		return true;					
+	}
+//Searches 
+	/**
+	 * This method tests to see if the array possess a value v
+	 * @param x: array to be tested
+	 * @param n: length of array
+	 * @param v: value to search for
+	 * @return index of value or -1 if not found
+	 */
+	public static int find(int[] x, int n, int v) {
+		//binary sort
+		int index1 = 0;//lower index							
+		int index2 = n - 1;//higher index
+		int mid; //center
+		while(index2 >= index1){
+			mid = (index1 + index2)/2;//find midpoint of the two index
+			if(x[mid] == v){//return if mid is the location of v
+				return mid;
+			}
+			if (x[mid] > v ){//set new upper bound if v is less than mid
+				index2 = mid-1;
+			}
+			else{//set new lower bound if v is greater than mid
+				index1 = mid+1;
+			}
+		}
+		return (-1);//value not found
+	}
+	
+	/**\
+	 * This method returns a new array of length n+1 with the value v inserted: non decreasing order
+	 * @param x array to copy and insert
+	 * @param n length of array
+	 * @param v value to add
+	 * @return array of n+1 length if v is not within the array already, length n if already present
+	 */
+	
+	public static int[] insertGeneral(int[] x, int n, int v) {
+		//creates new array and initializes variables
+		 int[] r = new int[n+1];
+		 int index;
+		 int flag=0;
+		 if(n==0){//checks for edge case of n=0
+			 r[n]=v;
+			 return r;
+		 }
+		 for(index = 0; index < n; index++){ //cycle through all index
+			 if(x[index]==v){//checks for v and return a copy of array if found
+				 return(Arrays.copyOfRange(x, 0,n));
+			 }
+			 if(flag == 1){//shifts storage of x data by + 1 in new array r if v is placed
+				 r[index+1] = x[index];
+			 }
+			 else{//inputs data from x into r
+				 r[index] = x[index];
+			 if(x[index]>v){//sets placed flag when v's location is found
+				 r[index] = v;
+				 r[index+1] = x[index];
+				 flag = 1;
+			 }
+			 }
+		 }
+		 if(x[n-1] < v){//checks for edge condition where v> all current array values
+			 r[n] = v;
+		 }
+		 return r;
+		}
+	
+	/**
+	 * This method edits the original array to insert the value v within it: non decreasing order
+	 * @param x array to edit
+	 * @param n length of array
+	 * @param v value to insert
+	 * @return
+	 */
+	
+	public static int insertInPlace(int[] x, int n, int v){
+		int index;
+		//checks for v
+		//modification of insertion sort
+		if(find(x,n,v)!= -1){
+			return n;
+		}
+		
+		for(index = n; index > 0; index--) {//cycles from last array value to first
+			if(x[index-1] < v) {//places v when location is found
+				x[index] = v;
+				break;
+			}
+			x[index] = x[index - 1];//shifts all data over 1 index until v location is found 
+		}
+		if(index==0){// checks for edge condition that v is < all values in array
+			x[0] = v;
+		}
+		return n+1;
+	}
+/**
+ * Sorts an array in non-decreasing order
+ * @param x array to be sorted 
+ * @param n length of the array
+ */
+	public static void insertSort(int[] x, int n){
+		//insert sort algorithm
+		for(int i = 1; i<n; i++){
+			int v = x[i];//sets current value to be sorted
+			int k;//location to sort from 
+			for(k = i - 1; k>=0; k--){//shifts all data over 1 index until current value is sorted
+				if(x[k] <= v){
+					break;
+				}
+				x[k+1] = x[k];//shift data by 1 index
+			}
+			x[k+1] = v;
+		}
+	}
+}
+	
