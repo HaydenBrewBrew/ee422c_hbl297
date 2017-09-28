e44a48179920a79c8cdc5bb24b08ac93cafff110
HaydenBrewBrew <haydenlydick@utexas.edu>
2017-09-28 15:22:48 -0500

    THIS ISNT EVEN MY FINAL FORM

--- Project2/src/assignment2/GameConfiguration.java
+++ Project2/src/assignment2/GameConfiguration.java
@@ -5,7 +5,7 @@
 package assignment2; 
 
 public class GameConfiguration {
-	public static final int guessNumber = 12;
-	public static final String[] colors = {"B","G","O","P","R","Y"};
+	public static final int guessNumber = 6;
+	public static final String[] colors = {"Q","W","E","R","T","Y"};
 	public static final int pegNumber = 4;
 }
